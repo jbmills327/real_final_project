365da5599e55657ea51c9b9d4528bb83fd355ba8
Justin Mills <jbmills@gmail.com>
2016-11-14 16:03:55 -0700

    front-end and back-end routing done for all html files

--- routes.js
+++ routes.js
@@ -5,9 +5,17 @@ var Auth = require("./controllers/auth"),
 var app = express();
 
 module.exports = (app) => {
+
+    app.post("/login", Auth.login);
     app.get("/logout", Auth.logout);
     app.post("/register", Auth.register);
-    app.post("/login", Auth.login);
+
+    app.get('/', Auth.session);
+
+
+    // anythin below line is protected
+    app.all('/api*', Auth.session);
+
 
     // set the home/root route and send it off to the appropriate handler
     app.get('/', (req, res) => {
@@ -15,29 +23,39 @@ module.exports = (app) => {
             root: './public/html'
         });
     });
-
+    // Backend routing for index.html
     app.get("/index", (req, res) => {
         res.sendFile("index.html", {
             root: "./public/html"
         });
     });
+    // Backend routing for fantasy.html
     app.get("/fantasy", (req, res) => {
         res.sendFile("fantasy.html", {
             root: "./public/html"
         });
     });
+    // Backend routing for horror.html
     app.get("/horror", (req, res) => {
         res.sendFile("horror.html", {
             root: "./public/html"
         });
     });
+    // Backend routing for scifi.html
     app.get("/scifi", (req, res) => {
         res.sendFile("scifi.html", {
             root: "./public/html"
         });
     });
+    // Backend routing for love.html
     app.get("/love", (req, res) => {
         res.sendFile("love.html", {
+            root: "./public/html"
+        });
+    });
+    // Backend routing for kids.html
+    app.get("/kids", (req, res) => {
+        res.sendFile("kids.html", {
             root: "./public/html"
         });
     });
